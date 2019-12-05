defd66b54e01df7a39a6728752bd21074d7f7939
Arick <wang2019031@gmail.com>
2019-12-05 11:56:47 +0800

    Updates

---
+++ precache-manifest.487db985fb7cf6132edbca4954f4ecfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3264e27cc9bea83b69e3b98666b923",
+    "url": "/react-snake/index.html"
+  },
+  {
+    "revision": "8e60833f10e3f7968db0",
+    "url": "/react-snake/static/css/main.9dc487de.chunk.css"
+  },
+  {
+    "revision": "4edb8bce678ac7eec0ad",
+    "url": "/react-snake/static/js/2.bf38212d.chunk.js"
+  },
+  {
+    "revision": "8e60833f10e3f7968db0",
+    "url": "/react-snake/static/js/main.e46e96c0.chunk.js"
+  },
+  {
+    "revision": "bdd4f9046507df7151ce",
+    "url": "/react-snake/static/js/runtime-main.07115961.js"
+  }
+]);
